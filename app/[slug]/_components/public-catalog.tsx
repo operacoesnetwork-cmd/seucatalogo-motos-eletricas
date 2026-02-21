2b26697f1ffb24a7be222dda973f9b3178bbf511
Operações Network <operacoes.network@gmail.com>
2026-02-21 09:38:11 -0300

    bug correction: script do pixel

--- app/[slug]/_components/public-catalog.tsx
+++ app/[slug]/_components/public-catalog.tsx
@@ -341,20 +341,31 @@ export function PublicCatalog({ store }: PublicCatalogProps) {
     <>
       {/* Tracking Scripts */}
       {store.metaPixelId && (
-        <Script id="meta-pixel" strategy="afterInteractive">
-          {`
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '${store.metaPixelId}');
-            fbq('track', 'PageView');
-          `}
-        </Script>
+        <>
+          <Script id="meta-pixel" strategy="afterInteractive" dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '${store.metaPixelId}');
+              fbq('track', 'PageView');
+            `
+          }} />
+          <noscript>
+            <img
+              height="1"
+              width="1"
+              style={{ display: "none" }}
+              src={`https://www.facebook.com/tr?id=${store.metaPixelId}&ev=PageView&noscript=1`}
+              alt=""
+            />
+          </noscript>
+        </>
       )}
 
       {store.googleAdsId && (
@@ -363,14 +374,14 @@ export function PublicCatalog({ store }: PublicCatalogProps) {
             strategy="afterInteractive"
             src={`https://www.googletagmanager.com/gtag/js?id=${store.googleAdsId}`}
           />
-          <Script id="google-analytics" strategy="afterInteractive">
-            {`
+          <Script id="google-analytics" strategy="afterInteractive" dangerouslySetInnerHTML={{
+            __html: `
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
               gtag('config', '${store.googleAdsId}');
-            `}
-          </Script>
+            `
+          }} />
         </>
       )}
 
